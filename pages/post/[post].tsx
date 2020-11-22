fc8baaa853337cdc8135f11aae1263612ad23e6e
metin <metin.ozkn@hotmail.com>
2020-11-22 17:23:31 +0300

    updated typeScript config

--- pages/post/[post].tsx
+++ pages/post/[post].tsx
@@ -1,24 +1,40 @@
 import { Typography, Grid, Divider } from "@material-ui/core";
 
+export type PostType = {
+  id: number;
+  title: string;
+  content: string;
+  published_at: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export type ParamsType = {
+  params: {
+    post: number;
+  };
+};
+
 export async function getStaticPaths() {
   const res = await fetch(`http://localhost:1337/posts`);
   const posts = await res.json();
-  console.log("res", res.json());
-  const paths = posts.map((post) => ({
+  const paths = posts.map((post: PostType) => ({
     params: { post: post.id.toString() },
   }));
 
   return { paths, fallback: false };
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({ params }: ParamsType) {
   const res = await fetch(`http://localhost:1337/posts/${params.post}`);
   const post = await res.json();
 
   return { props: { post } };
 }
 
-function Post({ post }) {
+function Post({ post }: { post: PostType }) {
+  console.log("res", post);
+
   return (
     <Grid item xs={12} md={8}>
       <Typography variant="h6" gutterBottom>
